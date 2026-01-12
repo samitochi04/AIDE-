c6c754844aba8d8da871e6b6aa1fe9a012599a61
samitochi04 <temmodaryl317@example.com>
2026-01-12 21:57:43 +0100

    push error

--- client/src/pages/auth/Login/Login.jsx
+++ client/src/pages/auth/Login/Login.jsx
@@ -129,7 +129,7 @@ export function Login() {
             )}
 
             {/* Social Login */}
-            <button
+            {/* <button
               type="button"
               className={styles.socialBtn}
               onClick={handleGoogleLogin}
@@ -141,7 +141,7 @@ export function Login() {
                 <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"/>
               </svg>
               {t('auth.continueWithGoogle')}
-            </button>
+            </button> */}
 
             <div className={styles.divider}>
               <span>{t('common.or')}</span>
